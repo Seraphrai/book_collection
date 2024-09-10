26c0f87e2c383a0b744fedf3bbf17b05e5d1a020
Seraphrai <84433119+Eragonlink@users.noreply.github.com>
2024-09-09 21:01:29 -0500

    User, User_book view and functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :user_books
+  resources :users
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
+  root 'user_books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
